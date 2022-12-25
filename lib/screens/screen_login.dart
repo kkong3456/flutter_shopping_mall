2756d7280a7a192c7c3684b20ca84ef06b252f4e
kkong7 <kkongk@naver.com>
2022-12-25 21:04:45 +0900

    ui

--- lib/screens/screen_login.dart
+++ lib/screens/screen_login.dart
@@ -5,8 +5,94 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('login')),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('로그인 화면'),
+        ),
+        body: Column(
+          children: [
+            emailInput(),
+            const PasswordInput(),
+            const LoginButton(),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
+            const RegisterButton(),
+          ],
+        ));
+  }
+}
+
+Widget emailInput() {
+  return Container(
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (email) {},
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+          labelText: '이메일',
+          helperText: '',
+        ),
+      ));
+}
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (password) {},
+        obscureText: true,
+        decoration: const InputDecoration(
+          labelText: '비밀번호',
+          helperText: '',
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+        ),
+        onPressed: () {},
+        child: const Text('로그인'),
+      ),
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return TextButton(
+      onPressed: () {
+        Navigator.of(context).pushNamed('/register');
+      },
+      child: Text(
+        '이메일로 간단하게 회원가입하기',
+        style: TextStyle(color: theme.primaryColor),
+      ),
     );
   }
 }
--- lib/screens/screen_register.dart
+++ lib/screens/screen_register.dart
@@ -6,10 +6,101 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Register'),
+      appBar: AppBar(
+        title: const Text('회원가입화면'),
       ),
+      body: Column(
+        children: const [
+          EmailInput(),
+          PasswordInput(),
+          PasswordConfirmInput(),
+          RegisterButton(),
+        ],
+      ),
+    );
+  }
+}
+
+class EmailInput extends StatelessWidget {
+  const EmailInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (email) {},
+        keyboardType: TextInputType.emailAddress,
+        decoration: const InputDecoration(
+          labelText: '이메일',
+          helperText: '',
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordInput extends StatelessWidget {
+  const PasswordInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (password) {},
+        obscureText: true,
+        decoration: const InputDecoration(
+          labelText: '비밀번호',
+          helperText: '',
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordConfirmInput extends StatelessWidget {
+  const PasswordConfirmInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: TextField(
+        onChanged: (password) {},
+        obscureText: true,
+        decoration: const InputDecoration(
+          labelText: '비밀번호 확인',
+          helperText: '',
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterButton extends StatelessWidget {
+  const RegisterButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      height: MediaQuery.of(context).size.height * 0.05,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
+          onPressed: () {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                const SnackBar(content: Text('회원가입이 완료되었습니다.')),
+              );
+            Navigator.pop(context);
+          },
+          child: const Text('회원가입')),
     );
   }
 }
