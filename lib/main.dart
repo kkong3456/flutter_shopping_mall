556f798470c68620cc243524d39594e5fc4537bd
kkong7 <kkongk@naver.com>
2022-12-25 22:02:04 +0900

    multidex 오류 해결

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_mall/models/model_auth.dart';
 import 'package:flutter_shopping_mall/screens/screen_detail.dart';
 import 'package:flutter_shopping_mall/screens/screen_index.dart';
 import 'package:flutter_shopping_mall/screens/screen_login.dart';
 import 'package:flutter_shopping_mall/screens/screen_register.dart';
 import 'package:flutter_shopping_mall/screens/screen_search.dart';
 import 'package:flutter_shopping_mall/screens/screen_splash.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //shared preferences 초기화
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,17 +21,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Shopping mall',
-      routes: {
-        '/': (context) => const SplashScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/index': (context) => const IndexScreen(),
-        '/register': (context) => const RegisterScreen(),
-        '/search': (context) => const SearchScreen(),
-        '/detail': (context) => const DetailScreen(),
-      },
-      initialRoute: '/',
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => FirebaseAuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Shopping mall',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/index': (context) => const IndexScreen(),
+          '/register': (context) => const RegisterScreen(),
+          '/search': (context) => const SearchScreen(),
+          '/detail': (context) => const DetailScreen(),
+        },
+        initialRoute: '/',
+      ),
     );
   }
 }
