3134f8aa8c41c3ea7f66626143e21145f07431b8
kkong7 <kkongk@naver.com>
2022-12-27 06:29:17 +0900

    register complete

--- lib/screens/screen_register.dart
+++ lib/screens/screen_register.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping_mall/models/model_auth.dart';
+import 'package:flutter_shopping_mall/models/model_register.dart';
+import 'package:provider/provider.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('회원가입화면'),
-      ),
-      body: Column(
-        children: const [
-          EmailInput(),
-          PasswordInput(),
-          PasswordConfirmInput(),
-          RegisterButton(),
-        ],
+    return ChangeNotifierProvider(
+      create: (_) => RegisterFieldModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('회원가입화면'),
+        ),
+        body: Column(
+          children: const [
+            EmailInput(),
+            PasswordInput(),
+            PasswordConfirmInput(),
+            RegisterButton(),
+          ],
+        ),
       ),
     );
   }
@@ -26,10 +32,14 @@ class EmailInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final registerField =
+        Provider.of<RegisterFieldModel>(context, listen: false);
     return Container(
       padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
       child: TextField(
-        onChanged: (email) {},
+        onChanged: (email) {
+          registerField.setEmail(email);
+        },
         keyboardType: TextInputType.emailAddress,
         decoration: const InputDecoration(
           labelText: '이메일',
@@ -45,10 +55,14 @@ class PasswordInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final registerField =
+        Provider.of<RegisterFieldModel>(context, listen: false);
     return Container(
       padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
       child: TextField(
-        onChanged: (password) {},
+        onChanged: (password) {
+          registerField.setPassword(password);
+        },
         obscureText: true,
         decoration: const InputDecoration(
           labelText: '비밀번호',
@@ -64,14 +78,20 @@ class PasswordConfirmInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final registerField = Provider.of<RegisterFieldModel>(context);
     return Container(
       padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
       child: TextField(
-        onChanged: (password) {},
+        onChanged: (password) {
+          registerField.setPasswordConfirm(password);
+        },
         obscureText: true,
-        decoration: const InputDecoration(
+        decoration: InputDecoration(
           labelText: '비밀번호 확인',
           helperText: '',
+          errorText: registerField.password != registerField.passwordConfirm
+              ? '비밀번호가 일치하지 않습니다.'
+              : null,
         ),
       ),
     );
@@ -83,6 +103,10 @@ class RegisterButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authClient =
+        Provider.of<FirebaseAuthProvider>(context, listen: false);
+    final registerField =
+        Provider.of<RegisterFieldModel>(context, listen: false);
     return Container(
       width: MediaQuery.of(context).size.width * 0.85,
       height: MediaQuery.of(context).size.height * 0.05,
@@ -92,13 +116,26 @@ class RegisterButton extends StatelessWidget {
               borderRadius: BorderRadius.circular(30.0),
             ),
           ),
-          onPressed: () {
-            ScaffoldMessenger.of(context)
-              ..hideCurrentSnackBar()
-              ..showSnackBar(
-                const SnackBar(content: Text('회원가입이 완료되었습니다.')),
-              );
-            Navigator.pop(context);
+          onPressed: () async {
+            await authClient
+                .registerWithEmail(registerField.email, registerField.password)
+                .then((registerStatus) {
+              if (registerStatus == AuthStatus.registerSuccess) {
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    const SnackBar(content: Text('회원가입이 완료되었습니다.')),
+                  );
+
+                Navigator.pop(context);
+              } else {
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(const SnackBar(
+                    content: Text('회원가입에 실패했습니다. 다시 시도해주세요'),
+                  ));
+              }
+            });
           },
           child: const Text('회원가입')),
     );
